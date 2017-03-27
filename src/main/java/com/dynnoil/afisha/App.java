08a11be9d5a0c71ac9f837080d69a6e07a54d64e
dynnoil <dynnoil@ya.ru>
2017-03-27 22:56:03 +0300

    Added Assets and Handlebars modules. Added bootstrap.

--- src/main/java/com/dynnoil/afisha/App.java
+++ src/main/java/com/dynnoil/afisha/App.java
@@ -2,6 +2,9 @@ package com.dynnoil.afisha;
 
 import com.dynnoil.afisha.modules.vk.VK;
 import org.jooby.Jooby;
+import org.jooby.Results;
+import org.jooby.assets.Assets;
+import org.jooby.hbs.Hbs;
 import org.jooby.pac4j.Auth;
 import org.pac4j.oauth.client.VkClient;
 
@@ -11,16 +14,19 @@ import org.pac4j.oauth.client.VkClient;
 public class App extends Jooby {
 
     {
+        use(new Hbs());
 
-        use(new VK());
+        use(new Assets());
+
+        /*use(new VK());
 
         use(new Auth().client(config -> {
             final String key = config.getString("vk.app_id");
             final String secret = config.getString("vk.client_secret");
             return new VkClient(key, secret);
-        }));
+        }));*/
 
-        get("/", () -> "Hello, world!");
+        get("/", request -> Results.html("index").put("greeting", "Hello, World!"));
     }
 
     public static void main(final String[] args) {
